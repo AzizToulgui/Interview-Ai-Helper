08ea9158a1e4abfd497b74dc76445f6fc98784b9
Aziz <aziztoulgui20@gmail.com>
2025-03-30 14:38:44 +0100

    feat : Finish the auth pages

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -4,33 +4,50 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
 import {z} from "zod";
 import { Button } from "@/components/ui/button"
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import {Form} from "@/components/ui/form"
 import Image from "next/image";
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-})
+import Link from "next/link";
+import {toast} from "sonner";
+import FormField from "@/app/components/FormField";
+import {useRouter} from "next/navigation";
 
 
+const authFormSchema = (type : FormType)=>{
+    return z.object({
+        name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(3),
+    })
+}
 
-const AuthForm = () => {
+const AuthForm = ({type} : {type: FormType}) => {
+    const router = useRouter();
+    const formSchema = authFormSchema(type);
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver : zodResolver(formSchema),
         defaultValues : {
-            username: "",
+            name: "",
+            email : "",
+            password: ""
         },
     });
     function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values);
+        try {
+            if(type === "sign-up"){
+                toast.success("Account created successfully please sign in!.");
+                router.push("/sign-in");
+            }
+            else {
+                toast.success("Sign up successfully!.");
+                router.push("/");
+            }
+        }catch (error) {
+            console.log(error);
+            toast.error(`There was an error : ${error}`)
+        }
     }
+    const isSignIn = type === "sign-in";
 
     return (
         <div className="card-border lg:min-w-[566px]">
@@ -42,10 +59,36 @@ const AuthForm = () => {
                 <h3 className="">Practice Job Interviews with AI</h3>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form ">
-                    <p>UserName</p>
-                    <Button type="submit">Submit</Button>
+                    {!isSignIn && (
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            label="Name"
+                            placeholder="Your Name"/>
+                    )}
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        label="Email"
+                        placeholder="Example@example.com"
+                        type="email"
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        label="Password"
+                        placeholder="Your Password"
+                        type="password"
+                    />
+                    <Button className="btn" type="submit">{isSignIn ? 'Sign in' : 'Create an Account'}</Button>
                 </form>
             </Form>
+                <p className="text-center">
+                    {isSignIn ? 'No account yet ?' : 'Have an account already'}
+                    <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">
+                        {!isSignIn ? 'Sign in' : 'Sign up'}
+                    </Link>
+                </p>
         </div>
         </div>
 
--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import {FormControl, FormDescription, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {FormControl, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
+import {Control, Path,Controller, FieldValues} from "react-hook-form";
 
-const FormField = () => (
-    <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-            <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                    This is your public display name.
-                </FormDescription>
-                <FormMessage />
-            </FormItem>
-        )}
+interface FormFieldProps<T extends FieldValues> {
+    control : Control<T>;
+    name : Path<T>;
+    label: string;
+    placeholder?: string;
+    type?: 'text' | 'email' | 'password' | 'file';
+}
+
+const FormField = ({control, name, label, placeholder, type = "text"} : FormFieldProps<T>) => (
+    <Controller
+        control={control}
+        name={name }
+        render={({field }) =>
+            (
+                <FormItem>
+                    <FormLabel className="label">{label}</FormLabel>
+                    <FormControl>
+                        <Input className="input" placeholder={placeholder} type={type} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+    )}
     />
 )
 export default FormField
